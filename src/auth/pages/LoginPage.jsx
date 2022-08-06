1de65ea13fc362746acf925fdd87901c8676771e
Agustin <re_agustin@hotmail.com>
2022-08-06 01:36:17 -0300

    06082022

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,10 +1,15 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom"
+import { AuthContext } from "../context";
 
 
 export const LoginPage = () => {
 
+  const {login} = useContext(AuthContext)
   const navigate = useNavigate();
+
   const onLogin = () => {
+    login('Juan Carlos');
     navigate('/', {replace: true});   
   }
 
